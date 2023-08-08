1672dcd36c7cc22d7178de6d343b217d47f7e781
GeorgeClinton56 <georgeclint56@gmail.com>
2023-08-08 20:59:50 +0100

    adding po.html

---
+++ index.js
@@ -0,0 +1,14 @@
+let js= "amazing"
+            if (js=="amazing");
+            console.log(3+5+2);
+            let firstname="jonas"
+            console.log(firstname)
+
+            let country="nigeria";
+            let continent="africa";
+            let population="200 ";
+
+            console.log(country);
+            console.log(continent);
+            console.log(population )
+            
